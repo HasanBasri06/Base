ce601c001b37919f53e289312fb628d08ce3f329
Hasan Basri <69991414+HasanBasri06@users.noreply.github.com>
2022-10-14 02:25:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ini_set("display_errors", 1);
 define('DIR', $_SERVER["DOCUMENT_ROOT"]."/base/");
 define('PHP_BASE_LINK', $_SERVER["REQUEST_URI"]);
 
-require DIR . "/vendor/autoload.php";
+require DIR . "vendor/autoload.php";
 
 $reqGet = isset($_GET["path"]) ? $_GET["path"] : PHP_BASE_LINK;
 
