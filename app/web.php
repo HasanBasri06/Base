ef2f6cf8e6ee0bc3f5b58451424135e6c3683709
Hasan Basri <HasanBari06>
2022-10-14 14:32:29 +0300

    change route system

--- app/web.php
+++ app/web.php
@@ -3,11 +3,15 @@
 use App\Controller\WelcomeController;
 
 return [
-    "deneme" => [
+    "/" => [
+        "controller" => WelcomeController::class,
+        "method" => "index"
+    ],
+    "/deneme" => [
         "controller" => WelcomeController::class,
         "method" => "index"        
     ],
-    "users" => [
+    "/users" => [
         "controller" => WelcomeController::class,
         "method" => "user"
     ]
